4eb46f3fb64bd7292d23162e3962bdbf0de01a27
artem.kuzmenko <artemkalolkalo@gmail.com>
2020-02-15 20:34:30 +0200

    alphabet

--- 00/1.c
+++ 00/1.c
@@ -1,9 +1,189 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-	printf("ggggggggggggggggg"\n");
-	printf("%s\n", "hello vaelr");
+	int main(void)
+
+{   
+	int nb;
+	char sb;
+
+	/* manascii */
+	printf("%-10s|%10s\n", "character", "ascii number");
+
+	               /* letter a  */
+	sb = 'a';
+	nb = 'a';
+	printf("%-10c|%10d\n", sb, nb);
+
+	                 /* letter b */
+	sb = 'b';
+	nb = 'b';
+	printf("%-10c|%10d\n", sb, nb);
+    
+                     /* letter c */
+    sb = 'c';
+	nb = 'c';
+	printf("%-10c|%10d\n", sb, nb);
+	
+	                 /* letter d */
+	sb = 'd';
+	nb = 'd';
+	printf("%-10c|%10d\n", sb, nb);
+
+	                /* letter e */
+	sb = 'e';
+	nb = 'e';
+	printf("%-10c|%10d\n", sb, nb);
+
+		            /* letter f */
+	sb = 'f';
+	nb = 'f';
+	printf("%-10c|%10d\n", sb, nb);
+
+		           /* letter g */
+	sb = 'g';
+	nb = 'g';
+	printf("%-10c|%10d\n", sb, nb);
+
+		          /* letter h */
+	sb = 'h';
+	nb = 'h';
+	printf("%-10c|%10d\n", sb, nb);
+
+		          /* letter i */
+	sb = 'i';
+	nb = 'i';
+	printf("%-10c|%10d\n", sb, nb);
+
+		           /* letter j */
+	sb = 'j';
+	nb = 'j';
+	printf("%-10c|%10d\n", sb, nb);
+
+		            /* letter k */
+	sb = 'k';
+	nb = 'k';
+	printf("%-10c|%10d\n", sb, nb);
+
+	         		/* letter l */
+	sb = 'l';
+	nb = 'l';
+	printf("%-10c|%10d\n", sb, nb);
+
+			       /* letter m */
+	sb = 'm';
+	nb = 'm';
+	printf("%-10c|%10d\n", sb, nb);
+
+			       /* letter n */
+	sb = 'n';
+	nb = 'n';
+	printf("%-10c|%10d\n", sb, nb);
+
+			       /* letter o */
+	sb = 'o';
+	nb = 'o';
+	printf("%-10c|%10d\n", sb, nb);
+
+		            /* letter p */
+	sb = 'p';
+	nb = 'p';
+	printf("%-10c|%10d\n", sb, nb);
+
+			        /* letter q */
+	sb = 'q';
+	nb = 'q';
+	printf("%-10c|%10d\n", sb, nb);
+
+			        /* letter r */
+	sb = 'r';
+	nb = 'r';
+	printf("%-10c|%10d\n", sb, nb);
+
+			        /* letter s */
+	sb = 's';
+	nb = 's';
+	printf("%-10c|%10d\n", sb, nb);
+
+			        /* letter t */
+	sb = 't';
+	nb = 't';
+	printf("%-10c|%10d\n", sb, nb);
+
+				     /* letter u */
+	sb = 'u';
+	nb = 'u';
+	printf("%-10c|%10d\n", sb, nb);
+
+			        /* letter v */
+	sb = 'v';
+	nb = 'v';
+	printf("%-10c|%10d\n", sb, nb);
+
+			        /* letter w */
+	sb = 'w';
+	nb = 'w';
+	printf("%-10c|%10d\n", sb, nb);
+
+			    	/* letter x */
+	sb = 'x';
+	nb = 'x';
+	printf("%-10c|%10d\n", sb, nb);
+
+				    /* letter y */
+	sb = 'y';
+	nb = 'y';
+	printf("%-10c|%10d\n", sb, nb);
+
+			    	/* letter z */
+	sb = 'z';
+	nb = 'z';
+	printf("%-10c|%10d\n", sb, nb);
+
+					/* number 1 */
+	sb = '1';
+	nb = '1';
+	printf("%-10c|%10d\n", sb, nb);
+
+					/* number 2 */
+	sb = '2';
+	nb = '2';
+	printf("%-10c|%10d\n", sb, nb);
+
+					/* number 3 */
+	sb = '3';
+	nb = '3';
+	printf("%-10c|%10d\n", sb, nb);
+
+					/* number 4 */
+	sb = '4';
+	nb = '4';
+	printf("%-10c|%10d\n", sb, nb);
+
+    				/* number 5 */
+	sb = '5';
+	nb = '5';
+	printf("%-10c|%10d\n", sb, nb);
+
+	    			/* number 6 */
+	sb = '6';
+	nb = '6';
+	printf("%-10c|%10d\n", sb, nb);
+
+    				/* number 7 */
+	sb = '7';
+	nb = '7';
+	printf("%-10c|%10d\n", sb, nb);
+
+    				/* number 8 */
+	sb = '8';
+	nb = '8';
+	printf("%-10c|%10d\n", sb, nb);
+ 
+   		    		/* number 9 */
+	sb = '9';
+	nb = '9';
+	printf("%-10c|%10d\n", sb, nb);
 
 	return (0);
 }
