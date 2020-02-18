202d2c6920de35bf483f8e1146d87e6be27ec404
artem.kuzmenko <artemkalolkalo@gmail.com>
2020-02-18 12:57:28 +0200

    size of letters in bytes

---
+++ 00/3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a;
+	float b;
+	double c;
+	char d;
+
+	printf("97= %lu\n", sizeof(a));
+
+    printf("98= %lu\n", sizeof(b));
+
+	printf("99= %lu\n", sizeof(c));
+
+    printf("100= %lu\n", sizeof(d));
+
+}
